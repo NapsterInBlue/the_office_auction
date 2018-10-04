c09a8b530ffc3634ad99fd97d7f89e71ac541b72
Nick Hounshell <NickHounshell@quickenloans.com>
2018-10-04 12:53:10 -0400

    Add selenium driver to get bid data

--- scraper.py
+++ scraper.py
@@ -3,11 +3,13 @@ from datetime import datetime
 import requests
 from bs4 import BeautifulSoup
 
+from selenium import webdriver
+
 now = datetime.now()
 TODAY = now.strftime('%Y%m%d')
 
 BASE_URL = r'https://auction.screenbid.com/view-auctions/catalog/id/105/'
-
+AUCTION_URL = r'https://auction.screenbid.com/'
 
 def get_no_verify(url):
     session = requests.Session()
@@ -16,9 +18,9 @@ def get_no_verify(url):
 
     return html
 
-def save_raw_page(item_name, html):
-    file_name = 'data/' + item_name + ' ' + TODAY + '.html'
-    with open(file_name, 'w') as f:
+def save_raw_page(item_name, html, extra=''):
+    file_name = 'data/' + item_name + ' ' + extra + ' ' + TODAY + '.html'
+    with open(file_name, 'w', encoding='utf-8') as f:
         f.write(html)
 
 
@@ -37,12 +39,43 @@ def get_next_page_url(html):
 
     return next_page_url
 
+def get_item_bid_url(html):
+    soup = BeautifulSoup(html, 'lxml')
+    bid_query_string = (soup.find('span', id='bidHistory')
+                            .find('a').get('href'))
+
+    item_bid_url = AUCTION_URL + bid_query_string
+    return item_bid_url
+
+def save_item_bid_history(item_name, url):
+    driver = webdriver.Chrome()
+    driver.get(url)
+
+    i = 1
+    while True:
+        html = driver.page_source
+        save_raw_page(item_name, html, extra=('bids ' + str(i)))
+        try:
+            next_page = driver.find_element_by_link_text('Next')
+            next_page.click()
+            i += 1
+        except:
+            break
+
+    driver.close()
+
 
 if __name__ == '__main__':
     html = get_no_verify(BASE_URL)
     soup = BeautifulSoup(html, 'lxml')
 
+    print(get_next_page_url(html))
+
     item_pages = get_item_pages(soup)
+
+
+
+
 
     first_page = item_pages[0]
 
@@ -51,4 +84,7 @@ if __name__ == '__main__':
 
     save_raw_page(first_page_name, first_page_html)
 
-    print(get_next_page_url(html))
+    bid_url = get_item_bid_url(first_page_html)
+    print(bid_url)
+
+    save_item_bid_history(first_page_name, bid_url)
